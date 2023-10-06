8f4613ac5671d4e1d05a3fe52a1e60f84f0b3f37
Bryan Chow <bchow.post@gmail.com>
2023-10-06 18:15:20 +0100

    Responsive + beginning of animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const header = document.querySelector(".title");
+
+header.addEventListener("mouseover", () => {
+	let { children } = header;
+	children = [...children];
+	children.forEach((item) => {
+		if (item.tagName.toLowerCase() === "span") {
+			item.classList.toggle("move");
+		}
+	});
+});
